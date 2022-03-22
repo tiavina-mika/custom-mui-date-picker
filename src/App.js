d858741be3ac64cb8be9421ae1c75c0f242880b5
TiavinaMichael <ralainirinatiavina@gmail.com>
2022-03-22 16:33:51 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import {
+  DateRangePicker,
+  DateRangeDelimiter,
+  LocalizationProvider
+} from "@material-ui/pickers";
+import DateFnsUtils from "@material-ui/pickers/adapter/date-fns"; // choose your lib
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  delimiter: {
+    marginLeft: 20,
+    marginRight: 20
+  }
+});
+
+export default function BasicDateRangePicker() {
+  const [selectedDate, setSelectedDate] = React.useState([null, null]);
+  console.log(selectedDate);
+  const classes = useStyles();
+  return (
+    <LocalizationProvider dateAdapter={DateFnsUtils}>
+      <DateRangePicker
+        startText=""
+        endText=""
+        value={selectedDate}
+        onChange={(newValue) => setSelectedDate(newValue)}
+        renderInput={(startProps, endProps) => (
+          <React.Fragment>
+            <TextField ref={startProps.inputRef} {...startProps} />
+            <div className={classes.delimiter}>-</div>
+            <TextField ref={endProps.inputRef} {...endProps} />
+          </React.Fragment>
+        )}
+      />
+    </LocalizationProvider>
+  );
+}
